4e0a966b72ff1f35923a8c912d7a21665ea49cf5
SunsJay <sunsjay0806@gmail.com>
2023-01-18 20:03:41 +0800

    feat: cos

--- src/pages/cos.tsx
+++ src/pages/cos.tsx
@@ -3,9 +3,11 @@ export const CardConfigCos = () => {
     <div className="flex justify-center">
       <form className="flex  m-4 max-w-2xl rounded-xl border border-gray-200 p-6 text-left text-inherit transition-colors hover:border-blue-600 hover:text-blue-600 focus:border-blue-600 focus:text-blue-600 active:border-blue-600 active:text-blue-600">
         <label htmlFor="secretId">Secret Id:</label>
-        <input className="ml-4" type="text" />
+        <input className="ml-4 border border-gray-200" type="text" />
+        <br />
+
         <label htmlFor="secretId">Secret Key:</label>
-        <input className="ml-4" type="text" />
+        <input className="ml-4 border border-gray-200" type="text" />
       </form>
     </div>
   )
