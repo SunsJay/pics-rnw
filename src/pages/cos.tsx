29dfd6aaad22b9e9265221c6b94d4917cabfb8b3
SunsJay <sunsjay0806@gmail.com>
2023-01-20 18:17:38 +0800

    refactor: css

--- src/pages/cos.tsx
+++ src/pages/cos.tsx
@@ -1,7 +1,7 @@
 export const CardConfigCos = () => {
   return (
-    <div className="flex justify-center">
-      <form className="flex  m-4 max-w-2xl rounded-xl border border-gray-200 p-6 text-left text-inherit transition-colors hover:border-blue-600 hover:text-blue-600 focus:border-blue-600 focus:text-blue-600 active:border-blue-600 active:text-blue-600">
+    <div className="flex  m-10 max-w-3xl rounded-xl border border-gray-200 p-6 text-left text-inherit transition-colors hover:border-blue-600 hover:text-blue-600 focus:border-blue-600 focus:text-blue-600 active:border-blue-600 active:text-blue-600">
+      <form>
         <label htmlFor="secretId">Secret Id:</label>
         <input className="ml-4 border border-gray-200" type="text" />
         <br />
